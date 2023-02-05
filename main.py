c4e6410245874519583edc0aee40ace0b13f65fd
Zelin Chen <6692czl@gmail.com>
2023-02-05 11:55:03 -0800

    A2-1

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
     ##################################################
     # Comlete your code here
+    Male = int(input())
+    Female = int(input())
+    Total = Male+Female
+    print('The total number of students:{0:10d}'.format(Total))
+    print('The numberof males and females:{0:>8d} {1:>8d}'.format(
+        Male, Female))
+    print('The percentage of males and females:{0:.2%} {1:.2%}'.format(
+        Male/Total, Female/Total))
+
     ##################################################
     pass
 
